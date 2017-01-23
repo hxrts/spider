c2c51b5b2e3b53746b9b26c9c097fb678e90abfb
hxrts <sam@hxrts.com>
2017-01-23 12:20:41 -0500

    additional webapp formatting

--- _ui.R
+++ _ui.R
@@ -11,21 +11,45 @@ UI <- function(origin, pop, direction, up.initial, depth, type) {
 		headerPanel('spider'),
 		fluidRow(
 
-			column(6, wellPanel(textInput(inputId = 'origin', label = 'origin channel ID, https://are.na/user/channel → channel', value = origin, placeholder = origin))),
+			column(6, wellPanel(
+				textInput(
+				inputId = 'origin',
+				label = 'Origin channel ID  ·  Are.na/sam-hart/research-tactics',
+				value = origin,
+				placeholder = origin))),
 
-			column(6, wellPanel(textInput(inputId = 'pop',    label = 'prune channels, comma separated IDs',               value = pop,    placeholder = pop))),
+			column(6, wellPanel(textInput(
+				inputId = 'pop',
+				label = 'Prune channels  ·  IDs comma separated',
+				value = pop,
+				placeholder = pop))),
 
-			column(2, wellPanel(selectInput(inputId = 'direction',  label = 'direction',       choices = c('down', 'up', 'down & up'), multiple = FALSE))),
+			column(2, wellPanel(selectInput(
+				inputId = 'direction',
+				label = 'Direction',
+				choices = c('down', 'up', 'down & up'),
+				multiple = FALSE))),
 
-			column(2, wellPanel(selectInput(inputId = 'up.initial', label = 'origin crawl up', choices = c('yes', 'no'),               multiple = FALSE))),
+			column(2, wellPanel(selectInput(
+				inputId = 'up.initial',
+				label = 'Origin crawl up',
+				choices = c('yes', 'no'),
+				multiple = FALSE))),
 
-			column(2, wellPanel(selectInput(inputId = 'depth', label = 'depth', choices = 1:4,                         multiple = FALSE))),
+			column(2, wellPanel(selectInput(
+				inputId = 'depth',
+				label = 'Depth', choices = 1:4,
+				multiple = FALSE))),
 
-			column(2, wellPanel(selectInput(inputId = 'type',  label = 'type',  choices = c('all', 'public', 'closed'), multiple = FALSE))),
+			column(2, wellPanel(selectInput(
+				inputId = 'type',
+				label = 'Type',
+				choices = c('all', 'public', 'closed'),
+				multiple = FALSE))),
 
-			column(1, wellPanel(actionButton('buildGraph', 'build'))),
+			column(1, wellPanel(actionButton('buildGraph', 'Build'))),
 
-			column(1, wellPanel(actionButton('clearGraph', 'clear'))),
+			column(1, wellPanel(actionButton('clearGraph', 'Clear'))),
 
 			column(12, visNetworkOutput('network_proxy', width = '100%', height = '1000px'))
 		)
